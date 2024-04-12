4e2819d08a586645f10e58a8cf508762c5def3ff
aaaddellle <53532361+aaaddellle@users.noreply.github.com>
2024-04-12 22:22:35 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import subprocess
+from datetime import datetime
+
+# Get timestamp for training
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# Run training on small test Imagen
+#subprocess.call(["venv/Scripts/python", "train.py", "-test", "-ts", timestamp])
+
+# Use small test Imagen to generate image
+#subprocess.call(["venv/Scripts/python", "inference.py", "-d", f"training_{timestamp}"])
+
+
+# Run training on small test Imagen
+subprocess.call(["python", "audio_changed\\train.py", "-test", "-ts", timestamp])
+
+# Use small test Imagen to generate image
+subprocess.call(["python", "audio_changed\\inference.py", "-d", f"training_{timestamp}"])
